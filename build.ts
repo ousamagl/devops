bc85523fbf8893f971580d334bb0a3a9c7d243ed
oussama galmami <oussama.galmami@gmail.com>
2021-01-26 15:58:05 +0100

    remove env

--- build.ts
+++ build.ts
@@ -4,6 +4,5 @@ const outDir = config.compilerOptions.outDir;
 
 s.rm('-rf', outDir);
 s.mkdir(outDir);
-s.cp('.env', `${outDir}/.env`);
 s.mkdir('-p', `${outDir}/common/swagger`);
 s.cp('./server/common/swagger/swagger.json', `${outDir}/common/swagger/swagger.json`)
