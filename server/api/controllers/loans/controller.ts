3343a7d08e9eb40aa2d07cbc8d4600e752f4e5d5
oussama galmami <oussama.galmami@gmail.com>
2021-01-28 18:14:42 +0100

    linter fixes

--- server/api/controllers/loans/controller.ts
+++ server/api/controllers/loans/controller.ts
@@ -2,11 +2,13 @@ import { Request, Response } from 'express';
 import BankService from '../../services/loan.service';
 export class Controller {
   create(req: Request, res: Response): void {
-    BankService.create(req.body.owner, req.body.ammount,req.body.status).then((r) =>
-      res.status(201).location(`/api/v1/loans`).json(r)
-    );
+    BankService.create(
+      req.body.owner,
+      req.body.ammount,
+      req.body.status
+    ).then((r) => res.status(201).location(`/api/v1/loans`).json(r));
   }
-  
+
   all(req: Request, res: Response): void {
     BankService.all().then((r) => res.json(r));
   }
